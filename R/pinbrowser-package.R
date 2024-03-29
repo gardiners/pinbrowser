5d6dac81c1766255c16438f4f58d407aeb6d26c6
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2024-03-29 18:48:20 +1100

    Set up imports

---
+++ R/pinbrowser-package.R
@@ -0,0 +1,15 @@
+#' Interactively browse pins and their metadata
+#'
+#' Browse the content of boards created with the pins package. View and search
+#' available pins, view their metadata, and generate code to read pins into your
+#' current project.
+#'
+#' @keywords internal
+#' @name pinbrowser
+NULL
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import shiny miniUI rstudioapi bslib htmltools
+## usethis namespace: end
+NULL
